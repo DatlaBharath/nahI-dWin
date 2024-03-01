2db7b2fb090789cad3504b83532cbb1b47f7ae36
DatlaBharath <datlabharath92@gmail.com>
2024-03-01 23:42:07 +0530

    done

--- server/app.js
+++ server/app.js
@@ -1,41 +1,49 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
+const cors = require('cors');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
+app.use(cors());
+const PORT = 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+const { Pool } = require('pg');
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'customers',
+  password: '12345678',
+  port: 5432,
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.get('/customers', async (req, res) => {
+  const { page = 1, limit = 20, search = '' } = req.query;
+  const offset = (page - 1) * limit;
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  try {
+    const query = {
+      text: `SELECT sno, customer_name, age, phone, location,
+                    TO_CHAR(created_at, 'YYYY-MM-DD') AS date,
+                    TO_CHAR(created_at, 'HH24:MI:SS') AS time
+             FROM customers
+             WHERE customer_name ILIKE $1 OR location ILIKE $1
+             ORDER BY sno
+             LIMIT $2 OFFSET $3`,
+      values: [`%${search}%`, limit, offset],
+    };
+
+    const { rows } = await pool.query(query);
+    console.log('fetch')
+    console.log(rows)
+    res.json(rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.listen(PORT, ()=>{
+  console.log(`Server running on port ${PORT}..`);
 });
 
 module.exports = app;
